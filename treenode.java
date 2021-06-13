1d0d0fc660df7be8f8213a9b778643bed08a04a8
mentariputri <72690333+mentariputri@users.noreply.github.com>
2021-06-13 08:53:06 -0700

    Add files via upload

---
+++ treenode.java
@@ -0,0 +1,19 @@
+package BinaryTree;
+public class treenode {
+    /*
+    a) B merupakan predesor dari A dan C
+    b) A dan C merupakan succesor dari B
+    c) B dan D merupakan ancestor dari C atau A
+    d) B dan C merupakan descendent dari D
+    e) B merupakan parent dari C
+    f) C merupakan child dari B
+    g) A dan C adalah sibling, E dan G adalah sibling
+    h) subtree B,A,C dan subtree F,E,G
+    */
+    char data;
+    treenode left;
+    treenode right;
+    public treenode(char data){
+        this.data = data;
+    }
+}
